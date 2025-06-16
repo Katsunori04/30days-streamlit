d2a14ea5863193e3e0d90ad1028efe41dda462dc
Katsunori04 <katsunori.fukui04@gmail.com>
2025-06-17 00:18:14 +0900

    Day 7とDay 8の内容を追加し、st.sliderの例を実装

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,39 @@ st.write(c)
 # day 6
 st.header("Day 6")
 st.write("Githubにアップロードした")
+
+# day 7
+st.header("Day 7")
+st.write("streamlit cloudにアップロードした")
+
+# day 8
+from datetime import time, datetime
+
+st.header("Day 8")
+st.write("st.slider")
+
+# 例1
+st.subheader("st.slider")
+
+age = st.slider("Select your age", 0, 130, 25)
+st.write("Your age is", age)
+
+# 例2
+st.subheader("Range slider")
+values = st.slider("Select a range of values", 0, 100, (25, 75))
+st.write("Values:", values)
+
+# 例3
+st.subheader("Range time slider")
+
+appointment = st.slider("Schedule your appointment", value=(time(11, 30), time(12, 45)))
+st.write("Your appointment is scheduled for", appointment)
+
+# 例4
+st.subheader("Datetime slider")
+start_time = st.slider(
+    "When do you start your day?",
+    value=(datetime(2020, 1, 1, 9, 30), datetime(2020, 1, 1, 17, 30)),
+    format="MM/DD/YY - hh:mm",
+)
+st.write("Start time:", start_time)
